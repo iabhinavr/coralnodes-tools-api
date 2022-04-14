d53454da75baa8036de1cdcd158d735aa52f6a98
Abhinav R <abhinav7r@gmail.com>
2022-04-14 21:38:47 +0530

    url validation added for critical css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const critical = require('critical');
+const Joi = require('joi');
 
 const app = new express();
 
@@ -11,7 +12,11 @@ app.use(express.json());
 app.post('/', async (req, res) => {
 
     try {
+
+        let validation = await validateUrl(req.body);
+
         let url = req.body.url;
+
         let response = await fetch(url);
         let responseText = await response.text();
         let length = responseText.length;
@@ -42,10 +47,26 @@ app.post('/', async (req, res) => {
     
 });
 
+function validateUrl(payload) {
+    const schema = Joi.object({
+        url: Joi.string().uri().required()
+    });
 
+    let { error } = schema.validate(payload);
+
+    return new Promise((resolve, reject) => {
+        if(!error) {
+            resolve('the url is valid');
+        }
+        else {
+            reject(`invalid url: ${error.details[0].message}`);
+        }
+    })
+
+}
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, 'localhost', () => {
     console.log(`Listening on port ${port}`);
 });
